bc0419299d6083319c80f474ca1b05e4340c4c89
archsinner <74122523+archsinner@users.noreply.github.com>
2024-02-23 21:41:09 -0500

    Updated ais
    Added new dependencies and created slstatus-desktop or laptop and prompts the user between the two to have a fully functional system regardless if youre on laptop or desktop. Laptop shows battery desktop does not.

--- ais.sh
+++ ais.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 # Display a welcome message using ncurses
-dialog --title "Welcome" --msgbox "Thanks for using archsinner's install script. This script installs a minimal suckless desktop, enjoy!" 10 40
+dialog --title "Welcome" --msgbox "Thanks for using archsinner's install script. This script updates Arch Linux, installs a minimal suckless desktop, and sets up dotfiles, enjoy!" 10 70
+
+# Update Arch Linux
+dialog --infobox "Updating Arch Linux..." 0 0
+sudo pacman -Syu --noconfirm
 
 # Function to check and install dependencies
 check_install_dependencies() {
-    local dependencies=(base-devel libx11 libxft xorg-server xorg-xinit terminus-font dialog libxinerama)
+    local dependencies=(xwallpaper base-devel libx11 libxft xorg-server xorg-xinit terminus-font dialog libxinerama xcompmgr webkit2gtk gcr exa wireplumber unclutter pipewire xdotool xcape)
     local missing_dependencies=()
     
     for dep in "${dependencies[@]}"; do
@@ -16,30 +20,24 @@ check_install_dependencies() {
     
     if [[ ${#missing_dependencies[@]} -gt 0 ]]; then
         # Install missing dependencies
-        dialog --title "Installing dependencies" --gauge "Installing required dependencies..." 10 50 0
-        sudo pacman -Sy "${missing_dependencies[@]}"
+        dialog --title "Installing dependencies" --gauge "Installing required dependencies..." 10 70 0
+        sudo pacman -Sy --noconfirm "${missing_dependencies[@]}"
     fi
 }
 
 # Check and install Git if not installed
 if ! command -v git &> /dev/null; then
     dialog --infobox "Git is not installed. Installing..." 0 0
-    sudo pacman -S git
-fi
-
-# Check and install Vim if not installed
-if ! command -v vim &> /dev/null; then
-    dialog --infobox "Vim is not installed. Installing..." 0 0
-    sudo pacman -S vim
+    sudo pacman -S --noconfirm git
 fi
 
 # Install dependencies
 check_install_dependencies
 
 # Prompt user for username and password
-dialog --inputbox "Enter a username:" 10 40 2> /tmp/username.txt
-dialog --passwordbox "Enter a password:" 10 40 2> /tmp/password.txt
-dialog --passwordbox "Confirm password:" 10 40 2> /tmp/password_confirm.txt
+dialog --inputbox "Enter a username:" 10 70 2> /tmp/username.txt
+dialog --passwordbox "Enter a password:" 10 70 2> /tmp/password.txt
+dialog --passwordbox "Confirm password:" 10 70 2> /tmp/password_confirm.txt
 
 # Read username and passwords from temporary files
 USERNAME=$(cat /tmp/username.txt)
@@ -48,7 +46,7 @@ PASSWORD_CONFIRM=$(cat /tmp/password_confirm.txt)
 
 # Check if passwords match
 if [ "$PASSWORD" != "$PASSWORD_CONFIRM" ]; then
-    dialog --msgbox "Passwords do not match. Please try again." 10 40
+    dialog --msgbox "Passwords do not match. Please try again." 10 70
     exit 1
 fi
 
@@ -70,25 +68,76 @@ rm /tmp/username.txt /tmp/password.txt /tmp/password_confirm.txt
 # Create .local/src directory in user's home directory
 sudo -u "$USERNAME" mkdir -p ~/"$USERNAME"/.local/src
 
-# Clone the repositories
-repos=(dwm st slstatus dmenu surf slock)
+# Create .local/bin directory in user's home directory
+sudo -u "$USERNAME" mkdir -p ~/"$USERNAME"/.local/bin
+
+# Prompt user for desktop or laptop usage
+dialog --title "Desktop or Laptop?" --yesno "Are you using a desktop or a laptop?" 10 70
+response=$?
+
+# Clone the appropriate slstatus repository based on user's choice
+if [ $response -eq 0 ]; then
+    # User selected desktop
+    dialog --infobox "Cloning slstatus-desktop repository..." 5 70
+    sudo -u "$USERNAME" git clone https://github.com/archsinner/slstatus-desktop.git /home/$USERNAME/.local/src/slstatus
+    # Install slstatus-desktop
+    dialog --infobox "Installing slstatus-desktop..." 5 70
+    (cd "/home/$USERNAME/.local/src/slstatus" && sudo -u "$USERNAME" make && sudo make install)
+else
+    # User selected laptop
+    dialog --infobox "Cloning slstatus-laptop repository..." 5 70
+    sudo -u "$USERNAME" git clone https://github.com/archsinner/slstatus-laptop.git /home/$USERNAME/.local/src/slstatus
+    # Install slstatus-laptop
+    dialog --infobox "Installing slstatus-laptop..." 5 70
+    (cd "/home/$USERNAME/.local/src/slstatus" && sudo -u "$USERNAME" make && sudo make install)
+fi
+
+# Remove original slstatus if it exists
+if [ -d "/home/$USERNAME/.local/src/slstatus" ]; then
+    dialog --infobox "Removing original slstatus repository..." 5 70
+    sudo -u "$USERNAME" rm -rf "/home/$USERNAME/.local/src/slstatus"
+fi
+
+# Clone the remaining repositories
+repos=(dwm st dmenu surf slock)
 total_repos=${#repos[@]}
 index=0
 
 for repo in "${repos[@]}"; do
     ((index++))
-    dialog --title "Cloning repository $repo ($index/$total_repos)" --infobox "Cloning $repo repository..." 5 50
-    sudo -u "$USERNAME" git clone "https://github.com/archsinner/$repo.git" "/home/$USERNAME/.local/src/$repo"
+    dialog --title "Cloning repository $repo ($index/$total_repos)" --infobox "Cloning $repo repository..." 5 70
+    if [ "$repo" == "slock" ]; then
+        sudo -u "$USERNAME" git clone "git@github.com:archsinner/slock-.git" "/home/$USERNAME/.local/src/slock"
+    else
+        sudo -u "$USERNAME" git clone "https://github.com/archsinner/$repo.git" "/home/$USERNAME/.local/src/$repo"
+    fi
 done
 
 # Compile and install each program
 for repo in "${repos[@]}"; do
-    dialog --title "Installing $repo" --infobox "Installing $repo..." 5 50
+    dialog --title "Installing $repo" --infobox "Installing $repo..." 5 70
     (cd "/home/$USERNAME/.local/src/$repo" && sudo -u "$USERNAME" make && sudo make install)
 done
 
+# Clone dotfiles repository and copy files to user's home directory
+dialog --infobox "Cloning dotfiles repository..." 5 70
+sudo -u "$USERNAME" git clone https://github.com/archsinner/dotfiles.git /home/$USERNAME/dotfiles
+
+# Copy dotfiles to user's home directory
+dialog --infobox "Copying dotfiles..." 5 70
+sudo -u "$USERNAME" cp -r /home/$USERNAME/dotfiles/.config /home/$USERNAME/
+sudo -u "$USERNAME" cp /home/$USERNAME/dotfiles/.xinitrc /home/$USERNAME/
+sudo -u "$USERNAME" cp /home/$USERNAME/dotfiles/.bashrc /home/$USERNAME/
+sudo -u "$USERNAME" cp /home/$USERNAME/dotfiles/.local/bin/remaps /home/$USERNAME/.local/bin/
+
+# Set ownership of copied files to the user
+sudo chown -R "$USERNAME:$USERNAME" /home/$USERNAME/.config /home/$USERNAME/.xinitrc /home/$USERNAME/.bashrc /home/$USERNAME/.local/bin/remaps
+
+# Set the remaps script to executable
+sudo chmod +x /home/$USERNAME/.local/bin/remaps
+
 # Display completion message
-dialog --msgbox "Suckless software installation completed!" 10 40
+dialog --msgbox "Suckless software installation and dotfiles setup completed!" 10 70
 
 # Exit
 clear
